571601a7d584d5940f1f134ab5347c5f065edcdd
alexander6273 <alex6273@stud.kea.dk>
2021-09-12 22:47:47 +0200

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello,:)  World!\n indskudt linje\n" +
+                "linje 2" +
+                "linje 3");
+        System.out.println("""
+                linje 1
+                linje 2
+                linje 3""");
+
+    }
+}
